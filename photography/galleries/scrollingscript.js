acf98b3b72f6abd4aaa6c92a00e9f0bac22c94c6
ori-des <151224246+ori-des@users.noreply.github.com>
2025-01-28 16:01:13 +0100

    Add forward scroll

--- photography/galleries/scrollingscript.js
+++ photography/galleries/scrollingscript.js
@@ -1,14 +1,27 @@
 const leftScrollBtn = document.getElementById('left-scroll');
 const rightScrollBtn = document.getElementById('right-scroll');
+const img = document.getElementById('image');
 
 leftScrollBtn.addEventListener('click', scroll);
 rightScrollBtn.addEventListener('click', scroll);
 
+let counter = 2;
 
 function scroll(event) {
-    if(event.target == leftScrollBtn) {
-        console.log('scrolling left');
-    } else {
-        console.log('scrolling right');
+    let src = img.src;
+    let idk = src.split("/")
+    let directory = "/photography/photos/" +  idk[idk.length - 2];
+    let nextImg = idk[idk.length - 2] + counter + ".jpg";
+    console.log(directory, nextImg);
+
+    if (counter > 18) {
+        return;
+    }
+
+    if(event.target == leftScrollBtn && counter > 2) {
+        console.log(counter);
+    } else if(event.target == rightScrollBtn) {
+        img.src = directory + "/" + nextImg;
+        counter++
     }
 }
